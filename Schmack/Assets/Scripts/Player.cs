329c0727dbd55b0dfacc52067209ed120447d129
Nick Rasmussen (RIT Student) <nlr3026@ad.rit.edu>
2019-09-25 18:10:43 -0400

    stuff

--- Schmack/Assets/Scripts/Player.cs
+++ Schmack/Assets/Scripts/Player.cs
@@ -4,28 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Vector3 position;
-    private Vector3 velocity;
-    private Vector3 acceleration;
+    private Vector2 position;
+    private Vector2 direction;
+    private Vector2 velocity;
+    private Vector2 acceleration;
+    [SerializeField] float speed;
     // Start is called before the first frame update
     void Start()
     {
         position = transform.position;
+        direction = Vector2.zero;
+        velocity = Vector2.zero;
+        acceleration = Vector2.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        velocity = Vector2.zero;
+        Gravity();
+        float h = Input.GetAxisRaw("Horizontal");
+        direction.x = h;
 
+        if (Input.GetButton("Jump"))
+        {
+            Debug.Log("A is pressed");
+        }
 
+        velocity += direction * speed * Time.deltaTime;
         position += velocity;
         transform.position = position;
     }
 
-
-    Vector3 AddForce(Vector3 force)
+    void Gravity()
     {
-        
+        acceleration.y -= 1;
     }
 }
