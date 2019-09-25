9be2303c4a258af01f8a3d7681678e7421e0ebe9
Nick Rasmussen (RIT Student) <nlr3026@ad.rit.edu>
2019-09-25 14:39:36 -0400

    started player movement

---
+++ Schmack/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 position;
+    private Vector3 velocity;
+    private Vector3 acceleration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+        position += velocity;
+        transform.position = position;
+    }
+
+
+    Vector3 AddForce(Vector3 force)
+    {
+        
+    }
+}
