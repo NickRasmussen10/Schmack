d54b408a24d6fe8b623d5d5f8d134fd2987f779f
Nick Rasmussen (RIT Student) <nlr3026@ad.rit.edu>
2019-09-26 23:43:24 -0400

    Made rayast length a function of the player's velocity
    Also commented a bunch of code

--- Schmack/Assets/Scripts/BowFiring.cs
+++ Schmack/Assets/Scripts/BowFiring.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class BowFiring : MonoBehaviour
 {
-    PlayerMovement playerMovement;
-    Vector3 direction;
-    [SerializeField] float knockback;
-    float powerInput;
+    PlayerMovement playerMovement;  //PlayerMovement script applied to the same GameObject as this script
+    Vector3 direction;  //unit vector, where the bow is pointing
+    [SerializeField] float knockback;   //the amount of force applied to the player when they fire the bow
+    float powerInput;   //keeps track of whether or not the bow's trigger button has been pressed
+
     bool isDrawnBack = false;
     // Start is called before the first frame update
     void Start()
@@ -19,18 +20,23 @@ public class BowFiring : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //store the right joystick input in direction vector
         direction.x = Input.GetAxis("RightHorizontal");
         direction.y = Input.GetAxis("RightVertical");
 
+        //store the trigger input in powerInput
         powerInput = Input.GetAxis("Fire1");
-        if(powerInput > 0.01f)
+
+        //give the bow a small deadzone
+        if(powerInput > 0.1f)
         {
             isDrawnBack = true;
         }
-        else if(powerInput < 0.01f && isDrawnBack)
+        //if bow has been released from previous draw
+        else if(powerInput < 0.1f && isDrawnBack)
         {
             isDrawnBack = false;
-            playerMovement.AddBowKnockback(-direction, knockback);
+            playerMovement.AddBowKnockback(direction, knockback);
         }
 
         Debug.DrawLine(transform.position, transform.position + (direction * 5f));
--- Schmack/Assets/Scripts/PlayerMovement.cs
+++ Schmack/Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,18 @@ public class PlayerMovement : MonoBehaviour
 {
     const float GRAVITY = 1.0f;
 
+    //physics vectors
     private Vector2 position;
     private Vector2 velocity;
     private Vector2 acceleration;
+
     [SerializeField] float mass;
-    [SerializeField] float speed;
-    [SerializeField] float jumpForce;
-    [SerializeField] float stickiness; //between 0 and 1
-    [SerializeField] float coefFriction;
+    [SerializeField] float speed;   //determines how much standard joystick controls move the player (horizontal)
+    [SerializeField] float jumpForce;   //determines how much power is behind the player's jump
+    [SerializeField] float stickiness; //between 0 and 1, lower = stickier, determines how much wall sticking slows the player's descent
+    [SerializeField] float coefFriction;    //determines how much friction effects the player
+
+    //flags
     bool isJumping = false;
     bool isSticking = false;
     bool isInAir = false;
@@ -29,24 +33,26 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Gravity();
-
+        //reset velocity
         velocity = Vector2.zero;
 
+        //environmental forces
+        Gravity();
+        AddFriction();
+        WallStick();
+
+        //player controls
         float h = Input.GetAxis("LeftHorizontal");
         velocity.x = h * speed;
 
         if (Input.GetButtonDown("Jump") && !isJumping)
         {
+            isJumping = true;
             acceleration.y = 0; //cancel out any previous forces
             AddForce(0, jumpForce);
-            isJumping = true;
         }
-        Debug.Log(acceleration);
 
-        AddFriction();
-        WallStick();
-
+        //application of forces
         velocity += acceleration * Time.deltaTime;
         position += velocity;
         transform.position = position;
@@ -57,22 +63,27 @@ public class PlayerMovement : MonoBehaviour
     /// </summary>
     void Gravity()
     {
-        int layerMask = ~(1 << 8); //excludes the player layer from raycasts
-        RaycastHit2D hit = Physics2D.Raycast(position, Vector2.down, 0.5f, layerMask);
+        //cast a ray downward from the player
+        LayerMask layerMask = LayerMask.GetMask("environment"); //specifies that only objects in the environment layer can be landed on
+        RaycastHit2D hit = CreateDynamicRaycast(Vector2.down, layerMask);
+
+        //if the ray hits a platform
         if (hit.collider != null)
         {
-            acceleration.y = 0;
             isJumping = false;
             isSticking = false;
             isInAir = false;
+
+            acceleration.y = 0;
+            //snap player to the top of the platform
             position.y = hit.collider.transform.position.y +
                 hit.collider.GetComponent<BoxCollider2D>().bounds.size.y / 2 +
                 gameObject.GetComponent<BoxCollider2D>().bounds.size.y / 2;
         }
         else if(!isSticking)
         {
-            acceleration.y -= GRAVITY;
             isInAir = true;
+            acceleration.y -= GRAVITY;
         }
         else
         {
@@ -80,34 +91,53 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Slows the player's downward velocity if they are next to a wall
+    /// </summary>
     void WallStick()
     {
+        //cast a ray to the left and right of the player
         LayerMask layerMask = LayerMask.GetMask("environment");
-        RaycastHit2D leftHit = CreateDynamicRaycast(Vector2.left, layerMask);
-        RaycastHit2D rightHit = CreateDynamicRaycast(Vector2.right, layerMask);
+        RaycastHit2D leftHit = CreateDynamicRaycast(Vector2.left, layerMask); //left
+        RaycastHit2D rightHit = CreateDynamicRaycast(Vector2.right, layerMask); //right
+
+        //if the ray hits a platform to the left
         if(leftHit.collider != null)
         {
-            if(acceleration.y > 0)
-            {
-                acceleration.y /= 1.3f;
-            }
             isJumping = false;
             isSticking = true;
-            float downwardForce = -GRAVITY * stickiness;
-            AddForce(0, downwardForce);
-            position.x = leftHit.collider.transform.position.x + 
-                leftHit.collider.GetComponent<BoxCollider2D>().bounds.size.x / 2 + 
-                gameObject.GetComponent<BoxCollider2D>().bounds.size.x / 2;
-        }
-        else if(rightHit.collider != null){
+
+            //slow down any leftover upward acceleration
             if (acceleration.y > 0)
             {
                 acceleration.y /= 1.3f;
             }
+
+            //apply a force downward
+            float downwardForce = -GRAVITY * stickiness;
+            AddForce(0, downwardForce);
+
+            //snap player to the right of the platform
+            position.x = leftHit.collider.transform.position.x + 
+                leftHit.collider.GetComponent<BoxCollider2D>().bounds.size.x / 2 + 
+                gameObject.GetComponent<BoxCollider2D>().bounds.size.x / 2;
+        }
+        //the the ray hits a platform to the right
+        else if(rightHit.collider != null){
             isJumping = false;
             isSticking = true;
+
+            //slow down any leftover upward acceleration
+            if (acceleration.y > 0)
+            {
+                acceleration.y /= 1.3f;
+            }
+
+            //apply a force downward
             float downwardForce = -GRAVITY + stickiness;
             AddForce(0, downwardForce);
+
+            //snap the player to the left of the platform
             position.x = rightHit.collider.transform.position.x -
                 rightHit.collider.GetComponent<BoxCollider2D>().bounds.size.x / 2 -
                 gameObject.GetComponent<BoxCollider2D>().bounds.size.x / 2;
@@ -118,6 +148,9 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Applies friction is thep layer is standing on a platform
+    /// </summary>
     void AddFriction()
     {
         if (!isInAir && Mathf.Abs(acceleration.x) > 0)
@@ -126,24 +159,45 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// applies force in opposite direction of bow fire
+    /// </summary>
+    /// <param name="direction">unit vector, the direction the bow is pointed in</param>
+    /// <param name="force">the amount of force applied by the knockback</param>
     public void AddBowKnockback(Vector2 direction, float force)
     {
-        AddForce(direction * force);
+        AddForce(-direction * force);
     }
 
+    /// <summary>
+    /// applies a force to acceleration
+    /// </summary>
+    /// <param name="force">the amount force applied to the player</param>
     void AddForce(Vector2 force)
     {
         acceleration += force / mass;
     }
+
+    /// <summary>
+    /// applies a force to acceleration
+    /// </summary>
+    /// <param name="x">amount of force applied in the x direction</param>
+    /// <param name="y">amount of force applied in the y direction</param>
     void AddForce(float x, float y)
     {
         acceleration.x += x / mass;
         acceleration.y += y / mass;
     }
 
+    /// <summary>
+    /// casts a ray with a size depenant on the player's current velocity
+    /// </summary>
+    /// <param name="direction">unit vector, the direction the ray will be casted in</param>
+    /// <param name="layerMask">LayerMask including all layers that should be checked by the Raycast</param>
+    /// <returns></returns>
     RaycastHit2D CreateDynamicRaycast(Vector2 direction, LayerMask layerMask)
     {
-        float distance = Mathf.Clamp(velocity.x * 2.5f, 0.5f, 2.0f);
+        float distance = Mathf.Clamp(velocity.sqrMagnitude * 2.5f, 0.5f, float.MaxValue);
         return Physics2D.Raycast(position, direction, distance, layerMask);
     }
 }
