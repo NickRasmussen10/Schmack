7c7a237c8d2a69eebe76aff304e19cedb605ddd0
Cori Mori (RIT Student) <cnm7453@ad.rit.edu>
2019-10-15 13:02:55 -0400

    Camera Stalker
    Added a script so the camera follows the player. Untested cause the the player doesnt move

---
+++ Schmack/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Cori Mori
+//10-15-19
+//Script that allows the camera to follow the player through the level
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;       // Reference to the player that the camera will follow
+
+    private Vector3 offset;         // Offset distance between the player and the camera
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Calculte and stroe the offset values by getting the distance between the player's position and the camera's position
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        //Set the position of the camera's transform to be the same as the player's plus the offset
+        transform.position = player.transform.position + offset;
+    }
+}
