e03214659adc27fd6d6866b8726e20853895bb97
Nick Rasmussen <nlr30262g.rit.edu>
2019-10-22 03:35:33 -0400

    i did something pretty neat

---
+++ Schmack/Assets/Scripts/PostProcessingTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class PostProcessingTest : MonoBehaviour
+{
+    [SerializeField] PostProcessingProfile flow;
+    [SerializeField] PostProcessingProfile noFlow;
+    [SerializeField] ParticleSystem rain;
+
+    bool inFlow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inFlow = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            inFlow = !inFlow;
+            if (inFlow)
+            {
+                gameObject.GetComponent<PostProcessingBehaviour>().profile = flow;
+                rain.enableEmission = false;
+            }
+            else
+            {
+                gameObject.GetComponent<PostProcessingBehaviour>().profile = noFlow;
+                rain.enableEmission = true;
+            }
+        }
+    }
+}
