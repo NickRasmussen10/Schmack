c353787ae9dc6957b4360680c970cfacd48b2d90
Nick Rasmussen (RIT Student) <nlr3026@ad.rit.edu>
2019-10-22 15:07:12 -0400

    walking enemy player tracking

--- Schmack/Assets/Scripts/Enemies/Walker.cs
+++ Schmack/Assets/Scripts/Enemies/Walker.cs
@@ -7,10 +7,14 @@ public class Walker : Enemy
     [SerializeField] GameObject start;
     [SerializeField] GameObject end;
     [SerializeField] float waitTime = 1.0f; //how long does he stay at each end of the range before turning around?
+    [SerializeField] float visionRange = 1.0f;
     float timer = 0.0f;
     float detectionRange = 0.5f; //how close does he need to get before he arrives at either end of the range
     GameObject target;
     Vector2 scale;
+
+    RaycastHit2D raycastHit;
+    bool seesPlayer = false;
 
     // Start is called before the first frame update
     new void Start()
@@ -19,6 +23,7 @@ public class Walker : Enemy
         target = end;
         timer = waitTime;
         direction = (target.transform.position - gameObject.transform.position).normalized;
+        direction.y = 0;
         scale = transform.localScale;
         scale.x = target == start ? scale.x : -scale.x;
         transform.localScale = scale;
@@ -28,33 +33,59 @@ public class Walker : Enemy
     new void Update()
     {
         base.Update();
+
+        raycastHit = Physics2D.Raycast(transform.position, direction, visionRange, LayerMask.GetMask("player"));
+        if(raycastHit.collider != null)
+        {
+            target = raycastHit.collider.gameObject;
+            seesPlayer = true;
+        }
+        Debug.DrawLine(transform.position, (Vector2)transform.position + (direction * visionRange));
         Move();
     }
 
 
     protected override void Move()
     {
-        float range = (target.transform.position - gameObject.transform.position).sqrMagnitude;
-        if (Mathf.Pow(target.transform.position.x - gameObject.transform.position.x, 2) < detectionRange * detectionRange)
+        if (!seesPlayer)
         {
-            //wait and swap direction
-            timer -= Time.deltaTime;
-            rb.velocity = Vector2.zero;
-            if(timer <= 0)
+            if (Mathf.Pow(target.transform.position.x - gameObject.transform.position.x, 2) < detectionRange * detectionRange)
             {
-                //swap targets
-                target = target == start ? end : start;
+                //wait and swap direction
+                timer -= Time.deltaTime;
+                rb.velocity = Vector2.zero;
+                if (timer <= 0)
+                {
+                    //swap targets
+                    target = target == start ? end : start;
 
-                direction = (target.transform.position - gameObject.transform.position).normalized;
-                scale.x *= -1;
-                transform.localScale = scale;
+                    direction = (target.transform.position - gameObject.transform.position).normalized;
+                    direction.y = 0;
+                    scale.x *= -1;
+                    transform.localScale = scale;
 
-                timer = waitTime;
+                    timer = waitTime;
+                }
+            }
+            else
+            {
+                //seek target
+                rb.AddForce(direction * acceleration * (Mathf.Abs(target.transform.position.x - gameObject.transform.position.x)));
+                rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), rb.velocity.y);
             }
         }
         else
         {
-            //seek target
+            Debug.Log("whaddup, son?");
+            //seek player
+            direction = (target.transform.position - transform.position).normalized;
+            direction.y = 0;
+            if((direction.x > 0 && scale.x > 0) || (direction.x < 0 && scale.x < 0))
+            {
+                scale.x *= -1;
+                transform.localScale = scale;
+            }
+            transform.localScale = scale;
             rb.AddForce(direction * acceleration * (Mathf.Abs(target.transform.position.x - gameObject.transform.position.x)));
             rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed), rb.velocity.y);
         }
