d0294a111ed0ed767b89dfe60c02c5cd57aa87ce
Cori Mori <33163783+CoriMori@users.noreply.github.com>
2020-04-14 09:43:21 -0400

    animation updates

---
+++ Schmack/Assets/Scripts/Enemies/AutonomousAgent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutonomousAgent : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float mass;
+    [SerializeField] float maxSpeed;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Seek(target.position));
+    }
+
+    Vector3 Seek(Vector3 target)
+    {
+        return ((target - transform.position) * maxSpeed * Time.deltaTime) / mass;
+    }
+}
